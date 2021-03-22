9088a3e7adc2cd6377174d063e218ac52f623c15
unknown <l.bouwknegt@st.hanze.nl>
2021-03-22 15:37:54 +0100

    added a recursive function to loop through the parsed data and get the ul elements

--- src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/webcrawling/DrugFetcher.java
+++ src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/webcrawling/DrugFetcher.java
@@ -7,29 +7,42 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
 public class DrugFetcher {
+    static List<Element> uppergroup;
+    static HashMap<String, List<String>> drugGroups = new HashMap<>();
+    static HashMap<String, List<String>> medicines = new HashMap<>();
 
-
-    public static void parseDrugs() throws IOException {
+    public static void main(String[] args) throws IOException {
         SSLHelper.bypassSSL();
         String url = "https://www.farmacotherapeutischkompas.nl/bladeren/categorie/psychiatrie";
         Document doc = Jsoup.connect(url).get();
-        Elements groups = doc.getElementsByClass("pat-rich group-2").select("h2");
-        HashMap<String, List<String>> drugGroups = new HashMap<>();
-        HashMap<String, List<String>> medicines = new HashMap<>();
-        for (Element h2Tag : groups
-             ) {
-            Elements subgroups = h2Tag.siblingElements().select("h3");
-            drugGroups.put(h2Tag.text(), subgroups.eachText());
-            for (Element subgroup: subgroups
-                 ) {
-                List<String> drugs = subgroup.siblingElements().select("li").eachText();
-                medicines.put(subgroup.text(), drugs);
-                }
+        uppergroup = doc.getElementsByClass("pat-rich group-2").select("h2");
+        checkUl(uppergroup);
+        System.out.println(drugGroups);
+        System.out.println(medicines);
+
+        }
+
+        private static void checkUl(List<Element> uppergroup){
+            Element h2Tag = uppergroup.get(0);
+            if (h2Tag.nextElementSibling().is("ul")) {
+                medicines.put(h2Tag.text(),h2Tag.nextElementSibling().select("li").eachText());
+            } else {
+                String query = h2Tag.nextElementSibling().tagName() + ":not(ul)";
+//                System.out.println(h2Tag.text() + h2Tag.nextElementSiblings().select(query).eachText());
+                drugGroups.put(h2Tag.text(),h2Tag.nextElementSiblings().select(query).eachText());
+                Element sibling = h2Tag.nextElementSibling();
+                uppergroup.add(sibling);
             }
+            uppergroup.remove(0);
+            if (uppergroup.size() != 0) {
+                checkUl(uppergroup);
+            }
+
         }
     }
 
