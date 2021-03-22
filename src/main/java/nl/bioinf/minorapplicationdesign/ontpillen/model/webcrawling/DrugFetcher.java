9dba1d611ae6b3b83d0f7f36ef3b7f7b5a836659
unknown <l.bouwknegt@st.hanze.nl>
2021-03-22 16:09:51 +0100

    Finished the implementation, cleaned the code

--- src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/webcrawling/DrugFetcher.java
+++ src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/webcrawling/DrugFetcher.java
@@ -7,42 +7,35 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class DrugFetcher {
     static List<Element> uppergroup;
     static HashMap<String, List<String>> drugGroups = new HashMap<>();
     static HashMap<String, List<String>> medicines = new HashMap<>();
 
-    public static void main(String[] args) throws IOException {
+    public static void parseDrugs() throws IOException {
         SSLHelper.bypassSSL();
         String url = "https://www.farmacotherapeutischkompas.nl/bladeren/categorie/psychiatrie";
         Document doc = Jsoup.connect(url).get();
         uppergroup = doc.getElementsByClass("pat-rich group-2").select("h2");
         checkUl(uppergroup);
-        System.out.println(drugGroups);
-        System.out.println(medicines);
-
         }
 
         private static void checkUl(List<Element> uppergroup){
-            Element h2Tag = uppergroup.get(0);
-            if (h2Tag.nextElementSibling().is("ul")) {
-                medicines.put(h2Tag.text(),h2Tag.nextElementSibling().select("li").eachText());
+            Element tag = uppergroup.get(0);
+            if (tag.nextElementSibling().is("ul")) {
+                medicines.put(tag.text(),tag.nextElementSibling().select("li").eachText());
             } else {
-                String query = h2Tag.nextElementSibling().tagName() + ":not(ul)";
-//                System.out.println(h2Tag.text() + h2Tag.nextElementSiblings().select(query).eachText());
-                drugGroups.put(h2Tag.text(),h2Tag.nextElementSiblings().select(query).eachText());
-                Element sibling = h2Tag.nextElementSibling();
-                uppergroup.add(sibling);
+                String query = tag.nextElementSibling().tagName() + ":not(ul)";
+                drugGroups.put(tag.text(),tag.nextElementSiblings().select(query).eachText());
+                Elements sibling = tag.nextElementSiblings().select(query);
+                uppergroup.addAll(sibling);
             }
             uppergroup.remove(0);
             if (uppergroup.size() != 0) {
                 checkUl(uppergroup);
             }
-
         }
     }
 
